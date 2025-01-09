4beeba619d6852aaa22b51907eab09d42617f7ef
Edward Burbank <142043578+Eddie-Burbank@users.noreply.github.com>
2025-01-09 13:54:39 +0000

    Dildofox v128 file upload

--- autoconfig.js
+++ autoconfig.js
@@ -1,5 +1,5 @@
 //
-pref("general.config.filename", "DildoFox.cfg");
+pref("general.config.filename", "dildofox.cfg");
 pref("general.config.obscure_value", 0);
 pref("general.config.sandbox_enabled", false);
 
